6fc5669c9667ed983dd0a416ddbf61446503b826
Onur Şentürk (ETIYA) <onur.senturk@etiya.com>
2022-10-17 16:40:51 +0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DataService } from './data.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pwa_angular';
+  joke : any;
+
+  constructor(private dataService :DataService){
+
+  }
+
+  ngOnInit(){
+    this.getJokes();
+  }
+
+  reloadPage() {
+    this.getJokes();
+  }
+
+  getJokes(){
+    this.dataService.getJokes().subscribe(res =>{
+      this.joke = res;
+    })
+  }
+
 }
